3c859aacddf08d41cdce1047de2bc1546314c3fd
thiago-cipitelli <cipitelliprata@gmail.com>
2023-12-16 12:19:24 -0300

    feat: add seeds

--- db/migrate/20231004210447_create_exercises.rb
+++ db/migrate/20231004210447_create_exercises.rb
@@ -2,10 +2,9 @@ class CreateExercises < ActiveRecord::Migration[5.2]
   def change
     create_table :exercises do |t|
       t.string :name
-      t.string :description
-      t.string :muscles
-
-      t.timestamps
+      t.string :muscle
+      t.string :equipment
+      t.float :rating
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'rubyXL'
+
+Exercise.destroy_all
+
+workbook = RubyXL::Parser.parse 'gymdataset.xlsx'
+worksheets = workbook.worksheets
+
+lista_exercicios = []
+
+worksheets.each do |worksheet|
+  worksheet.each do |row|
+    exercicio = {
+      name: row[0].value,
+      muscle: row[5].value,
+      equipment: row[7].value,
+      rating: row[8].value
+    }
+
+    lista_exercicios << exercicio
+  end
+end
+
+Exercise.create!([lista_exercicios])
+
+p "Created #{Exercise.count} exercises"
